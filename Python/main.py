10dd2a34ff89f6d90ce2fff619d0a4e119942dd9
NAVIN-STAR <navinacharya2000@gmail.com>
2024-12-19 23:52:02 +0530

    nabin first commit

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,20 @@
-print("hello")
+def main():
+    n = 1111
+    rem = 0
+    num = n
+    unique_divisors = set()
+    
+    while n > 0:
+        rem = n % 10
+        if rem == 0:
+            n //= 10
+            continue
+        if num % rem == 0:
+            unique_divisors.add(rem)
+        n //= 10
+
+    # Output the size of the set
+    print(len(unique_divisors))
+
+if __name__ == "__main__":
+    main()
